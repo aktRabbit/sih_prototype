306d383be6a7856f75a0d6f796ed0f027146aa0f
Ankit Singh <ankit6480@gmail.com>
2018-03-09 02:27:03 +0530

    aa

---
+++ config/keys.js
@@ -0,0 +1,14 @@
+module.exports = {
+        facebook: {
+            clientID: '178741589402133',
+            clientSecret: '631e4083a5bb6fd04570fecabf5691ce'
+        },
+        mongodb: {
+            dbURI: 'mongodb://akt_rabbit:haha@ds251807.mlab.com:51807/akt_rabbit',
+            dbURI1:'mongodb://ankit1:Akt,1998@ds261128.mlab.com:61128/andhra_pradesh'
+
+        },
+        session: {
+            cookieKey: 'theakt_rabbit'
+        }
+};
